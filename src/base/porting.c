ad3b3d1b94ca5f645f8719425e3db215b632c052
tw-kang <twkang@cubrid.com>
2026-09-11 18:32:38 +0900

    Revert "fork bench: release-only build break for the ticket 54 check (do not merge)"
    This reverts commit 40e19b855cd6cb767592ef849081e35d90fd39d7.

--- src/base/porting.c
+++ src/base/porting.c
@@ -22,11 +22,6 @@
 
 #ident "$Id$"
 
-/* gha-ci fork bench, ticket 54: a release-only build break. Never merged. */
-#if defined (NDEBUG) && !defined (CUBRID_OPTDEBUG)
-#error "gha-ci fork bench: deliberate release-only build break"
-#endif
-
 #include "config.h"
 
 #include <stdio.h>
